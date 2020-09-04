6b0e2c1088829dc74f1951e6151b8aa607516b3d
abdalla0 <42655471+abdalla0@users.noreply.github.com>
2020-09-03 20:18:10 -0400

    Initialzing Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to build machine learning models to classify the failure status of sensors and detect anomalies',
+    author='Mustafa Abdallah',
+    license='MIT',
+)
